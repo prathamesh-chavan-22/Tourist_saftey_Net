c2728e827a778316478ed2dc420800a5a1470ee7
Prathamesh Chavan <158864599+prathamesh-chavan-22@users.noreply.github.com>
2025-10-05 17:30:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ from passlib.context import CryptContext
 Base = declarative_base()
 
 # Password hashing context
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 class User(Base):
     __tablename__ = "users"
